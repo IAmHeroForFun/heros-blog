0ef794bdd7f9434f37ffe17cb9610a8bcd8ac924
IAmHeroForFun <everythinga1360@gmail.com>
2024-01-02 22:15:37 +0530

    added author

--- post/models.py
+++ post/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from ckeditor.fields import RichTextField
+from django.contrib.auth.models import User 
 
 class Category(models.Model): # blog category like tech/life/adventure
     name = models.CharField(max_length=255)
@@ -19,8 +20,9 @@ class Tag(models.Model): # tag the posts to different same posts
     
 class BlogPost(models.Model): # Posts from here 
     title = models.CharField(max_length=255)
-    content = RichTextField()
+    content = RichTextField() # using CKEditor which gives RichTextField
     pub_date = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, default=1) # for personal account posting only set default 1 
     categories = models.ManyToManyField(Category)
     tags = models.ManyToManyField(Tag)
     published = models.BooleanField(default=False)
