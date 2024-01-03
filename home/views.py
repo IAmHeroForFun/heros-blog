ce8a070f90a960494ef58bfce5f3fa45cf6059f9
IAmHeroForFun <everythinga1360@gmail.com>
2024-01-03 22:20:20 +0530

    added class based view for post list

--- home/views.py
+++ home/views.py
@@ -3,10 +3,18 @@ from django.urls import reverse
 from django.views import generic
 from django.template import loader
 
+from post.models import BlogPost
+
 
 # Will be updated to class based views so no comment for specifics details yet.
 def home(request):
     return render(request, 'index.html')
+
+class Blog(generic.ListView):
+    model = BlogPost
+    template_name = 'home/blog.html'
+    context_object_name = 'blogpost'
+    ordering = '-pub_date'
 
 
 def contact(request):
