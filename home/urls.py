bd7634e4dbd7cd4ce1a8f3973e9c2b439be93e44
IAmHeroForFun <everythinga1360@gmail.com>
2024-01-03 22:21:25 +0530

    home and blog seperate url

--- home/urls.py
+++ home/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path 
-from . import views 
+from . import views
+from .views import Blog
 
 app_name = 'home'
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('blog/', Blog.as_view(), name='blog'),
     path('contact/', views.contact, name='contact'),
     path('about/', views.about, name='about'),
     path('newsletter/', views.newsletter, name='newsletter'),
