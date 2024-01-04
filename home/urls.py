966b10e891bba8f718ccd03f9021686d41568281
IAmHeroForFun <everythinga1360@gmail.com>
2024-01-04 21:06:43 +0530

    contact class based view and url

--- home/urls.py
+++ home/urls.py
@@ -1,13 +1,14 @@
 from django.urls import path 
 from . import views
-from .views import Blog
+from .views import Blog, ContactView
 
 app_name = 'home'
 
 urlpatterns = [
     path('', views.home, name='home'),
     path('blog/', Blog.as_view(), name='blog'),
-    path('contact/', views.contact, name='contact'),
+    # path('contact/', views.contact, name='contact'),
+    path('contact/', ContactView.as_view(), name='contact'),
     path('about/', views.about, name='about'),
     path('newsletter/', views.newsletter, name='newsletter'),
 ]
--- home/views.py
+++ home/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import get_object_or_404, render
-from django.urls import reverse
+from django.urls import reverse_lazy
 from django.views import generic
 from django.template import loader
 
+from django.contrib import messages
+from .models import Contact
 from post.models import BlogPost
-
+from .forms import ContactForm
 
 # Will be updated to class based views so no comment for specifics details yet.
 def home(request):
@@ -17,9 +19,18 @@ class Blog(generic.ListView):
     ordering = '-pub_date'
 
 
-def contact(request):
-    return render(request, 'home/contact.html')
-
+# def contact(request):
+#     return render(request, 'home/contact.html')
+class ContactView(generic.CreateView):
+    model = Contact
+    form_class = ContactForm
+    template_name = 'home/contact.html'
+    success_url = reverse_lazy('home:contact')
+    
+    def form_valid(self, form):
+        response = super().form_valid(form)
+        messages.success(self.request, "Thank you for using me!!!")
+        return response
 
 def about(request):
     return render(request, 'home/about.html')
