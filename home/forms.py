8b99b7391c469a28a99d30a9c0286c251ee6d887
IAmHeroForFun <everythinga1360@gmail.com>
2024-01-09 22:29:35 +0530

    added search form

--- home/forms.py
+++ home/forms.py
@@ -4,14 +4,18 @@ from crispy_forms.helper import FormHelper
 from crispy_forms.layout import Submit
 
 
-class ContactForm(forms.ModelForm):
+class ContactForm(forms.ModelForm):  # Its to fill in from front-end
     class Meta:
-        model= Contact
+        model = Contact
         fields = ['name', 'email', 'subject', 'message']
-        
-    def __init__(self, *args, **kwargs): # Crispy form for better look on front-end with bootstraps5
+
+    # Crispy form for better look on front-end with bootstraps5
+    def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.helper = FormHelper()
         self.helper.form_method = 'post'
         self.helper.add_input(Submit('submit', 'Submit'))
-        
+
+
+class SearchForm(forms.Form):  # search data in the site with this
+    query = forms.CharField(label='Search')
